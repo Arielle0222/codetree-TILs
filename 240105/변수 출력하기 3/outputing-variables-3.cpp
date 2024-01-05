d5ac9e1780e75196221e987f15e690ad90cc2645
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-05 19:34:03 +0900

    Title: 변수 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240105/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    a = 1;
+    b = 2;
+    char c = 'C'
+
+
+    cout<<a<<"->"<<b<<"->"<<c;
+	return 0;
+
+}
