3f2eac3b6c85556da3aa9db08ab4f9ed636c13b0
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-05 19:37:57 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int w = 13;
+	double g = 0.165;
+
+	cout << fixed;
+	cout.precision(6);
+
+	cout << w << " * " << g << " = " << w*g;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
