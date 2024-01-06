2fbc946cb6514a90b965e56c8e5ceb2f06111bcf
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-06 21:53:41 +0900

    Title: 날짜 변경하여 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240106/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespacce std;
+
+int main() {
+    int y,m,d;
+    char s = "."
+    cin >> y;
+    cin.get();
+    cin >> m;
+    cin.get();
+    cin >> d;
+
+    cout << y << s << m << s << d;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
