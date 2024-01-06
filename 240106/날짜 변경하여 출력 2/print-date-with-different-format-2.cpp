39e9687ebbc555ee59e82b14576b9d6f29b42c1f
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-06 21:56:56 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
+++ 240106/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
     int y,m,d;
     char s = '.';
-    cin >> y;
-    cin.get();
     cin >> m;
     cin.get();
     cin >> d;
+    cin.get();
+    cin >> y;
 
     cout << y << s << m << s << d;
 
