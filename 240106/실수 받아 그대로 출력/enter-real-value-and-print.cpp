a66cc78c2b0236b867cea7e6fc90294fe2809064
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-06 21:24:22 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//실수는 double, 문자는char, 문자열은 string
+int main() {
+    double n;
+    
+    cout.fixed;
+    cout.precision(2);
+
+    cout << n;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
