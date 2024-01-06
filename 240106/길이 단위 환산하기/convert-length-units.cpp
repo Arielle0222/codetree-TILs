a9d0fdf41f2bbb7c31adb65c4e5c9a405fedaa39
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-06 21:27:52 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ft = 30.48;
+    double n;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << ft*n;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
