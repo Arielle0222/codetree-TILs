1ae44b0c2a85faf613edab0f91abde10f876f8cc
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-06 21:59:06 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x;
+    cin.get();
+    cin >> y;
+
+    cout << y << x;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
