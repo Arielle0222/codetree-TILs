83f9a85e4768cf6b104c712a066eb022f711d512
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-06 21:49:11 +0900

    Title: 1시간 뒤 시간 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240106/1시간 뒤 시간 출력/print-one-hour-later.cpp
+++ 240106/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
     int h,m;
-    cin >> h >> m;
+    cin >> h;
     cin.get() // 숫자 사이 문자열 무시
-
+    cin >> m;
+    
     cout << h+1 << m;
     // 여기에 코드를 작성해주세요.
     return 0;
