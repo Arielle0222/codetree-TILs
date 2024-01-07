a00f9807e9e79d29ec061150a293531e6672f2df
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-07 22:16:36 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240107/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
+++ 240107/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -5,8 +5,8 @@ int main() {
     int a,b;
     cin >> a >> b;
 
-    cout.fixed;
-    cout << precision(2);
+    cout << fixed;
+    cout.precision(2);
 
     cout << (double)(a+b) / (a-b);
     // 여기에 코드를 작성해주세요.
