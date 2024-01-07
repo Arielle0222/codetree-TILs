0e5d8069b38e98fc57ffd050a5d11d9483cd080f
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-07 22:09:34 +0900

    Title: 두수의 곱과 몫, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240107/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
+++ 240107/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     int a,b;
     cin >> a >> b;
 
-    cout << a << " * " << b = a*b;
+    cout << a << " * " << b = a*b << endl;
     cout << a << " / " << b = a/b;
     // 여기에 코드를 작성해주세요.
     return 0;
