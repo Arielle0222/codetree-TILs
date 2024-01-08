014493cd28f2aceb4e2c298795bf1c01bd25cf75
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-08 22:23:12 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240108/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s;
+    cin >> s;
+
+    s = (s == 100)? "pass" : "failure";
+
+    cout << s;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
