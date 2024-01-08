2eb1e1156ade99b435adbf066d51c5296b882369
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-08 22:18:02 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    if(a<b){
+        cout << b; 
+    }
+    else{
+        cout << a;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
