8a6dea0a2fd8f1d9bb6bc1cee376151ffb7b8f79
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-08 22:28:41 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/삼항연산자/ternary-operator.cpp
+++ 240108/삼항연산자/ternary-operator.cpp
@@ -5,6 +5,21 @@ int main() {
     int s;
     cin >> s;
 
+    string result = (s == 100)? "pass" : "failure";
+
+    cout << result;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s;
+    cin >> s;
+
     string result = (s == 100) ? "pass" : "failure";
 
     cout << result;
@@ -12,3 +27,5 @@ int main() {
     // 여기에 코드를 작성해주세요.
     return 0;
 }
+
+*/
