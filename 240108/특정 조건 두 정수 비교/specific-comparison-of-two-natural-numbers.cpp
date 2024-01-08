50df9ed0ae8df00f7fd83aa4aea4026108c73e13
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-08 22:50:43 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240108/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
+++ 240108/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -6,17 +6,17 @@ int main() {
 	cin >> a >> b;
 
 	if (a<b){
-		cout << 1;
+		cout << "1";
 	}
 	else{
-		cout << 0;
+		cout << "0";
 	}
 
 	if (a==b){
-		cout << 1;
+		cout << "1";
 	}
 	else{
-		cout <<0;
+		cout << "0";
 	}
     // 여기에 코드를 작성해주세요.
     return 0;
