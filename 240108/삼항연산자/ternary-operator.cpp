04633aaa6b7fb7d28ba9c74f3ddc73014c595a23
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-08 22:32:01 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/삼항연산자/ternary-operator.cpp
+++ 240108/삼항연산자/ternary-operator.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
     int s;
     cin >> s;
-
+    
+    //출력되는 값은 문자여! --> string으로 결과값 받아야 하지
     string result = (s == 100)? "pass" : "failure";
 
     cout << result;
