47ce10d0d0a08236122faeebdf3db427410ee679
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-10 16:38:46 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/윤년인가/is-leap-year.cpp
+++ 240110/윤년인가/is-leap-year.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    // 연도 입력 받기
+    // 변수 선언
     int y;
-    std::cin >> y;
 
-    // 윤년 판단
-    bool isLeapYear = false;
-
-    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
-        isLeapYear = true;
-    }
-
-    // 결과 출력
-    std::cout << std::boolalpha << isLeapYear << std::endl;
+	// 입력
+	cin >> y;
+    
+    // 출력
+    //1
+	if(y % 4 == 0) {
+        //2
+		if(y % 100 == 0) {
+            //3
+			if(y % 400 == 0) {
+				cout << "true";
+			}
+            ////////////////////
+			//3번 else
+            else {
+				cout << "false";
+			}
+		}
+        //2번 else
+		else {
+			cout << "true";
+		}
+	}
+    //나머지 경우에는 윤년이 아닙니다.
+    //1번 else
+	else {
+		cout << "false";
+	}
 
     return 0;
 }
