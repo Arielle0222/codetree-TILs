d39792162847c78e70301f0fa19f32cea289aca4
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-10 16:02:09 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int s,a;
+
+	cin >> s >> a;
+
+
+	if (s == 0) {
+		if (a >= 19) {
+			cout << "MAN";
+		}
+		else {
+			cout << "BOY";
+		}
+	}
+	if (s ==1){
+		if (a >= 19) {
+			cout << "WOMAN";
+		}
+		else {
+			cout << "GIRL";
+		}
+	}
+
+	return 0;
+
+}
