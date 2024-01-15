b5caa7a4f4f8e1fa6a75b29ca28834cbbcd7c95e
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-15 21:23:21 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240115/n까지 숫자 출력/print-1-to-n.cpp
+++ 240115/n까지 숫자 출력/print-1-to-n.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n,i;
     cin >> n;
 
-    while(n<=10){
-        cout << n << " ";
-        n+=1;
+    while(i<=n){
+        cout << i << " ";
+        i+=1;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
