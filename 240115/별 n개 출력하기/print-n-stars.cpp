cf96a0e446ab50dd1ade270671e9ff1c3ed87b57
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-15 21:28:35 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/별 n개 출력하기/print-n-stars.cpp
+++ 240115/별 n개 출력하기/print-n-stars.cpp
@@ -5,7 +5,7 @@ int main() {
     int n,i;
     cin >> n;
 
-    while(i<n){
+    while(i<n){ //i=1 초기 입력값 입력 안해서 여기 코드에서 i는 0부터 시작함. 그래서 i<n+1하면 별이 6개 찍히는 거임.
         cout << '*' << endl;
         i+=1;
     }
