c8b5d580a837470801e24d98d8753607ee3242da
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-15 21:27:41 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+    cin >> n;
+
+    while(i<n){
+        cout << '*' << endl;
+        i+=1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
