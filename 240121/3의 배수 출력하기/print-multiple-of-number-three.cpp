d03546f633b59b801f58820f540cdc6302f1e745
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-01-21 16:42:10 +0900

    Title: 3의 배수 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240121/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    while(int i<=n){
+        cout << i << " ";
+        i+=3;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
