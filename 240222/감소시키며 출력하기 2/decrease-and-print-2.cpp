985cb9483dbad6dc4abd333f509a23c3c89eeb51
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-02-22 15:24:56 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240222/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=26;
+
+    while(i>9){
+        cout << i << " ";
+        i-=1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
