1529edc6611b956fa4729fa9563b163e863de683
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-02-22 15:23:16 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240222/짝수만 출력/output-only-even.cpp
+++ 240222/짝수만 출력/output-only-even.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main() {
     int a,b;
-    int i=a;
+    int i; //
+    // int i=a; 이렇게 정의하면 안되는 이유는 변수 i를 사용자 입력값을 읽기 전에 a의 값으로 초기화시켜버려서임
 
     cin >> a >> b;
+    i=a; //여기서 i=a로 줘야함
+   
 
-    while(i<b+1){
+    while(i<=b){
         cout << i << " ";
         i+=2;
     }
