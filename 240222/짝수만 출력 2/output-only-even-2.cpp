f3295dfd4519c0534e187e81bf63dd1a537cc1ed
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-02-22 15:28:16 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,i;
+
+    cin >> a >> b;
+    i=a;
+
+    while(i>=10){
+        cout << i << " ";
+        i-=2;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
