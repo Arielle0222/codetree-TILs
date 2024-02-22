c782f913a886d813eb0fca9d19cee0de6788dc00
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-02-22 16:28:39 +0900

    Title: n번 더하기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/n번 더하기/add-n-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,n;
+    //int c;
+
+    cin >> a >> n;
+    //c=a+n;
+
+    for(int i=1; i<=n ; i++){
+        a +=n;
+        cout << i << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
