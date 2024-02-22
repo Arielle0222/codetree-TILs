cd1886740ef7e9994a01328cc4920ca2ebaffbe7
SeoHee Kim / 김서희 <114340019+Arielle0222@users.noreply.github.com>
2024-02-22 16:19:59 +0900

    Title: a/b 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240222/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cin >> a >> b;
+
+    cout << a/b << "."; //정수 부분 먼저 출력
+
+    a=a%b;
+
+    for(int i=0; i<20 ; i++){    
+        a*=10;
+        cout << a/b
+
+        a%=b;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
