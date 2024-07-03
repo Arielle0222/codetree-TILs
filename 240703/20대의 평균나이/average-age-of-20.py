741607dfb6e3c67bf8bf8f563443273942c454d8
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-03 21:22:23 +0900

    Title: 20대의 평균나이, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240703/20대의 평균나이/average-age-of-20.py
+++ 240703/20대의 평균나이/average-age-of-20.py
@@ -1,12 +1,18 @@
+# 변수 선언
+sum_val = 0
 cnt = 0
-sum_age = 0
 
+# 언제 끝날지 모르므로
+# 계속 반복합니다.
 while True:
+    # 변수를 선언하고 입력을 받습니다.
     n = int(input())
-    if n % 20 <=9 :
-        cnt+=1
-        sum_age+=n
 
-    elif n >=30 :
-        print(f"{sum_age/cnt:.2f}")
+    # 입력받은 값이 20대가 아니면 종료합니다.
+    if n >= 30 or n <= 19:
+        print(f"{sum_val/cnt:.2f}")
         break
+    
+    # 20대가 맞는 경우에는, 계속 값을 계산해줍니다.
+    sum_val += n
+    cnt += 1
