d58ce195eed5f2ceb01dfd33651052fd2f1684ed
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-03 11:44:23 +0900

    Title: 조건에 부합하는 수, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240703/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 240703/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -1,17 +1,7 @@
 a = int(input())
 
-def 조건을_만족하지_않는지(i):
-    if i % 2 == 0 and i % 4 != 0:
-        return False
-    if (i // 8) % 2 == 0:
-        return False
-    if i % 7 < 4:
-        return False
-    return True
-
-result = []
-for i in range(1, a + 1):
-    if 조건을_만족하지_않는지(i):
-        result.append(i)
-
-print(" ".join(map(str, result)))
+for i in range(1, a+1):
+	#continue -> a
+	if (i % 2 == 0 and i % 4 != 0) or (i // 8) % 2 == 0 or (i % 7) < 4:
+		continue
+	print(i, end=" ")
