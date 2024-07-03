459b9fe2322a5224b15fa3e99e3d12343cda4537
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-03 21:03:30 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240703/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,12 @@
+cnt = 0
+sum_age = 0
+
+while True:
+    n = int(input())
+    if n % 20 <=9 :
+        cnt+=1
+        sum_age+=n
+
+    elif n >=30 :
+        print(f"{sum_age/cnt:.2f}")    
+        break
