220ab0efff5152f9304531ecfb0eed1605b76024
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-04 20:53:12 +0900

    Title: ab 사이에 없는 c, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240704/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240704/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,12 +1,12 @@
 arr = input().split()
 a,b,c = int(arr[0]), int(arr[1]), int(arr[2])
 
-satisfied = True #배수가 있다!!.
+satisfied = True #응 다 없어.. 
 for i in range (a,b+1):
     if i % c == 0 :
-        satisfied = False # 배수가 없다.
+        satisfied = False # 하나는 있다 얘
 
 if satisfied == False :
-    print("NO")
+    print("NO") #그래 있다고
 else:
-    print("YES")
+    print("YES") # 없당께
