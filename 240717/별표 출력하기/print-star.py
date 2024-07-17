dcc385e0d67190d570d94650180badebf1a163a3
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-17 17:36:31 +0900

    Title: 별표 출력하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/별표 출력하기/print-star.py
@@ -0,0 +1,32 @@
+'''
+for i in range(3): 
+    for j in range(3 - i):
+        print("*", end="")
+    print()
+
+for i in range(1, -1, -1): 
+    for j in range(3 - i):
+        print("*", end="")
+    print()
+
+>>>
+***
+**
+*
+**
+***
+
+'''
+
+
+n = int(input())
+
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print() #3행 까지
+
+for i in range(3, -1, -1):
+    for j in range(i+1):
+       print("*", end=" ")
+    print() #3행 까지
