7c5bba252018a2e67c81696015208ea1d2167dd4
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-17 17:26:06 +0900

    Title: 직각 삼각형 출력, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240717/직각 삼각형 출력/Right-triangle-and-output.py
@@ -1,14 +1,14 @@
+'''
 n = int(input())
 
 for i in range(n):
     for j in range(2*i +1):
         print('*', end="")
     print()
-
 '''
+
 
 n = int(input())
 
 for i in range(1, n+1):
-    print('*' * (2*i - 1))
-'''
+    print('*' * (2*i - 1)) # *찍히는 규칙 수열 공식임
