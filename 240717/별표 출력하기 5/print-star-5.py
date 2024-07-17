69c73ac1a9f5dc07055e30826ed8e7b8b0193505
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-17 17:14:30 +0900

    Title: 별표 출력하기 5, Time: 257ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/별표 출력하기 5/print-star-5.py
+++ 240717/별표 출력하기 5/print-star-5.py
@@ -1,3 +1,14 @@
+'''
+n = int(input())
+
+for i in range(n):
+    for j in range(n-i):
+        for k in range(n-i):
+            print('*', end="")
+        print()
+'''
+
+
 n = int(input())
 
 for i in range(n):
