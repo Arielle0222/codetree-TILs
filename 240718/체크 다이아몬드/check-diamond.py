9af219a3bbeb697a6d4894fe642ddef06bc3776d
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-18 16:27:47 +0900

    Title: 체크 다이아몬드, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240718/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,31 @@
+n = int(input())
+'''
+for i in range(n):
+    for j in range(n-1-i):
+        print(" ", end= " ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+    
+for i in range(n-2, -1, -1):
+    for j in range(n-1-i):
+        print(" ", end= " ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+'''
+
+for i in range(n):
+    for j in range(n-1-i):
+        print(" ", end="")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+
+# Lower part of the diamond
+for i in range(n-2, -1, -1):
+    for j in range(n-1-i):
+        print(" ", end="")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
