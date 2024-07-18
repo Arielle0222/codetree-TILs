5fa99fae8d175101a75ab7dafe4e8238b5bec695
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-07-18 16:28:28 +0900

    Title: 체크 다이아몬드, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/체크 다이아몬드/check-diamond.py
+++ 240718/체크 다이아몬드/check-diamond.py
@@ -17,9 +17,9 @@ for i in range(n-2, -1, -1):
 
 for i in range(n):
     for j in range(n-1-i):
-        print(" ", end="")
+        print(" ", end="") #공백은 띄어쓰기가 아님
     for j in range(i+1):
-        print("*", end=" ")
+        print("*", end=" ") # *만 공백 띄어쓰기로 진행
     print()
 
 # Lower part of the diamond
