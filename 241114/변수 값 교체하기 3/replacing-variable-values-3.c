c36eab7fe5767d64c45365f24479d189d10cec01
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-14 16:54:47 +0900

    Title: 변수 값 교체하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241114/변수 값 교체하기 3/replacing-variable-values-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a = 3;
+    int b =5;
+    temp = a; //temp == 3 and a == 3 and b ==5
+    a = b; // temp == 3 and a == 5 and b == 5
+    b = temp // temp == 3 and. a == 5 b ==3
+
+    printf("%d\n%d",a,b);
+
+    return 0;
+}
+
