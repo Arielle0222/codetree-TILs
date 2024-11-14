fc973f7ebcaa35420dc33c668469337419cc6ef4
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-14 20:31:50 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/세 실수의 반올림/rounding-of-three-actual-numbers.c
+++ 241114/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     double a,b,c;
 
-    scanf("%lf %lf %lf",&a,7b,&c);
+    scanf("%lf %lf %lf",&a,&b,&c);
     printf("%.3lf\n%.3lf\n%.3lf",a,b,c);
     return 0;
 }
