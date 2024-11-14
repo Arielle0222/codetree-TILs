bcdc48d80684762ed6ccc8b29ccccd7153ec438f
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-14 19:55:40 +0900

    Title: 입력받은 값 교체하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/입력받은 값 교체하기/changing-inputs.c
+++ 241114/입력받은 값 교체하기/changing-inputs.c
@@ -3,11 +3,8 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
-    a = temp;
-    temp = b;
-    b = a;
 
     scanf("%d %d",&a,&b);
-    printf("%d %d",a,b);
+    printf("%d %d",b,a);
     return 0;
 }
