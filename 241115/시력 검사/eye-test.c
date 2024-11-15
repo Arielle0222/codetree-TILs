9c8194f256a6cfd959cb1e02fb9165aa4032e619
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-15 17:05:21 +0900

    Title: 시력 검사, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241115/시력 검사/eye-test.c
+++ 241115/시력 검사/eye-test.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 변수 선언
+    double a, b;
 
-    double a,b;
-
-    scanf("%lf %lf",a,b);
-
-    if (a>=1.0 && b>=1.0) {
-        printf("High");
-    }
-
-    else
-        printf("Low");
+	// 입력
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+    
+    // 출력
+	if(a >= 1.0 && b >= 1.0)
+		printf("High");
+	else if(a >= 0.5 && b >= 0.5)
+		printf("Middle");
+	else
+		printf("Low");
+	
     return 0;
 }
