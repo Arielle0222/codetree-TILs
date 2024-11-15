b6bd31f7532e096855977f64b7c01ace2b698a97
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-15 13:21:59 +0900

    Title: 큰 수에서 빼기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241115/큰 수에서 빼기/subtract-from-large-number.c
+++ 241115/큰 수에서 빼기/subtract-from-large-number.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    // 변수 선언
+    int a, b;
 
-	int a,b;
-	scanf("%d %d", &a,&b);
-
-   if (a>=b) {
-    printf("%d",a-b);
-   }
-   printf("%d", b-a);
-
-		return 0;
-
+	// 입력
+	scanf("%d %d", &a, &b);
+    
+    // 출력
+	if(a > b)
+		printf("%d", a - b);
+	if(a <= b)
+		printf("%d", b - a);
+    return 0;
 }
-
-
-
-
