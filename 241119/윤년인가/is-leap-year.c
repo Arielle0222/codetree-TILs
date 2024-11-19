091ccbd008ac868473fb71ecbdee7af5e597ce45
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-19 12:49:05 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/윤년인가/is-leap-year.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+
+    scanf("%d", &y);
+
+    if (y % 4 == 0 ) {
+        printf("true");
+    }
+    else if ((y % 100 == 0) && (y % 400 != 0)){
+            printf("false");
+        } 
+    else {
+        printf("false");
+    }
+    return 0;
+}
