6702ce08cb21b53536904fbe426a4524918db0a6
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-21 10:14:12 +0900

    Title: n부터 1까지 숫자 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241121/n부터 1까지 숫자 출력/print-n-to-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    
+    scanf("%d", &n);
+
+    while (i>=1) {
+        printf("%d ", i);
+        i--;
+    }
+    return 0;
+}
