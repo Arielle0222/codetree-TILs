504d7f9831bf45922f82c37a904796c950e7f49a
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-21 10:01:55 +0900

    Title: 짝수만 출력, Time: 1016ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

---
+++ 241121/짝수만 출력/output-only-even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a,b,i;
+
+    scanf("%d %d", &a, &b);
+
+    while (i<=b) {
+        printf("%d ", i);
+        i=a+2;
+    }
+    return 0;
+}
