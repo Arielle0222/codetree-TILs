9d4629368abbe1e618a16760cb67a7fe426d4fdf
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-22 15:53:41 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241122/친근하지 않은 수/unfriendly-number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt=0;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i<=n; i++){
+        if (i%2==0||i%3==0||i%5==0){
+            cnt++;
+        }
+    }
+
+    printf("%d", n-cnt);
+    return 0;
+}
+
+
+
+//continue는 for, while 안에서만 사용 가능함
+//continue는 if와 같이 쓰임 : 조건이 만족하면 아래 코드 실행 x
+//[결론]continue는 조건문 필수
