c68f1c56168b57af18a2ab65a1eeca6989697dda
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-25 17:03:49 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/몫 출력하기/print-share.c
+++ 241125/몫 출력하기/print-share.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    int i = 0; // 작업 횟수를 for 문에서 관리
-
-    while (i < 3) { // 작업을 최대 3번까지만 수행
+    // 여기에 코드를 작성해주세요.
+    int cnt=0;
+    
+    while (1){
+        int n;
         scanf("%d", &n);
 
-        if (n % 2 == 1) {
-            continue; // 홀수는 넘어감
-        } else {
-            // 짝수라면 작업 수행
-            printf("%d\n", n / 2);
-            i++; // 작업 횟수 증가
+        if (n%2==1){
+            continue;
+        }
+        else{
+            printf("%d\n", n/2);
+            cnt++;
+        }
+
+        if(cnt==3){
+            break;
         }
     }
-
     return 0;
 }
