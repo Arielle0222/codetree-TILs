e9e81a6ee0a98c6d48b4a04df55fae43aab47c06
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-25 20:38:28 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241125/공약수의 유무/presence-or-absence-of-a-common-divisor.c
+++ 241125/공약수의 유무/presence-or-absence-of-a-common-divisor.c
@@ -8,9 +8,9 @@ int main() {
 
     scanf("%d %d", &a, &b);
     
-    bool satisfied = false;//최대공약수가 존재하지 않는다는게 기본 전제 조건
+    bool satisfied = false;//공약수가 존재하지 않는다는게 기본 전제 조건
     for (int i=a; i<=b; i++){
-        if (1920%i==1 && 2880%i==2){
+        if (96%i==0){
             satisfied = true;
         }
     }
