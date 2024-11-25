90344858c864a41e9908a24d07934e581bb1217a
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-25 17:09:29 +0900

    Title: 20대의 평균 나이, Time: 1035ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241125/20대의 평균 나이/average-age-of-20.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum_val=0, cnt=0;
+
+    while (1){
+        int n;
+        
+        scanf("%d", &n);
+
+        if (n%20 <=9){
+            sum_val+=n;
+            cnt++;
+            continue;
+        }
+        else {
+            printf("%.2lf",(double)sum_val/cnt);
+            break;
+        }
+    }
+    return 0;
+}
