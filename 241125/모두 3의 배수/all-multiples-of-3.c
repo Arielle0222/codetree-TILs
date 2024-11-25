44c55ee60971a9eeabc4a57786edb655235798c3
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-25 21:30:11 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241125/모두 3의 배수/all-multiples-of-3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a,b,c,d,e;
+
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    bool satisfied = true; // 다 3의 배수라고 가정
+
+    for (int i=1; i<=5; i++){
+
+        int a;
+        scanf("%d", &a);
+
+        if (a%3!=0){
+            satisfied = false; //하나라도 3의 배수가 아닌게 나오면 0
+        }
+    }
+
+    if (satisfied == false) {
+        printf("0");
+    }
+    else {
+        printf("1");
+    }
+    return 0;
+
+}
