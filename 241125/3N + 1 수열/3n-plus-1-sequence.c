063135ad2763ba476aa294cb68d057c01f5abd21
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-25 17:32:23 +0900

    Title: 3N + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/3N + 1 수열/3n-plus-1-sequence.c
+++ 241125/3N + 1 수열/3n-plus-1-sequence.c
@@ -6,13 +6,17 @@ int main() {
     scanf("%d", &n);
     
     while (1){
-        if(n==1){
-            break;
-        }
+        //n이 1이 되는 경우 While문을 탈출해야 함.
+        //해당 조건문이 가장 마지막으로 가면 n==1일때에도
+        //continue문을 따라서 n%2==1 루프에서 계속 도니까 그보다 위에서 반복문을 실행해야 함
+        
         if(n%2==0){
             n=n/2;
             cnt++;
             continue;
+        }
+        if(n==1){
+            break;
         }
         if(n%2==1){
            n=n*3+1;
