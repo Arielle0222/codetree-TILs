a776e50df6ab2606c018d7ae116f79b2efdc1cf0
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-26 12:43:46 +0900

    Title: 비교에 따른 연산, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/비교에 따른 연산/operation-based-on-comparison.py
+++ 241126/비교에 따른 연산/operation-based-on-comparison.py
@@ -7,4 +7,24 @@ b = arr[1]
 if (a>b) :
     print(a*b)
 else :
-    print(b/a)
+    print(b//a)
+
+
+
+# python 사칙연산
+'''
+a = 7
+b = 3
+
+print(a + b)  # 10
+print(a - b)  # 4
+print(a * b)  # 21
+print(a / b)  # 2.3333333333333335 (float division) -> 실수로 나누기
+print(a // b) # 2 (floor division) -> 정수로 나누기
+print(a % b)  # 1 (modulus)
+'''
+
+
+
+# C 사칙연산
+#  + - * / %
