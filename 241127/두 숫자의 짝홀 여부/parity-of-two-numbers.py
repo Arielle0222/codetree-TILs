af3441a0ceb18605d68c909cd2a3419707437667
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 16:09:07 +0900

    Title: 두 숫자의 짝홀 여부, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241127/두 숫자의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+
+a = arr[0]
+b = arr[1]
+
+if (a%2==0) :
+    print("even")
+elif (a%2==1) :
+    print("odd")
+
+if (b%2==0) :
+    print("even")
+elif(b%2==1) :
+    print("odd")
