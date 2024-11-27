440b1a5f53f3fa3403992a6f99839c71ad937038
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 17:05:07 +0900

    Title: 장학금, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241127/장학금/scholarship.py
@@ -0,0 +1,12 @@
+# 변수 선언, 입력
+arr = list(map(int, input().split()))
+m = int(arr[0])
+f = int(arr[1])
+
+# 출력
+if (m >= 90 and f >= 95):
+	print("100000")
+elif (m >= 90 and f >= 90):
+	print("50000")
+else:
+	print("0")
