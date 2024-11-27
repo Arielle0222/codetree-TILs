3eb27cfb84d3d2e85d91f29582af4f6dfdd0c619
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 16:26:52 +0900

    Title: 4번의 크기 비교, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241127/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,13 @@
+
+a = int(input())
+
+arr = list(map(int, input().split()))
+b = arr[0]
+c = arr[1]
+d = arr[2]
+e = arr[3]
+
+print(int(a > b))
+print(int(a > c))
+print(int(a > d))
+print(int(a > e))
