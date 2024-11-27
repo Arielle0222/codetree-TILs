83a1a2a8088e86f2b1f4847491913c341679bd91
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 21:01:38 +0900

    Title: 높은 수에서 낮은 수까지, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241127/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+a = arr[0]
+b = arr[1]
+
+if (a<b) :
+    for i in range(b, a-1, -1) :
+        print(i, end=" ")
+elif (a>b) :
+    for i in range(a, b-1, -1) :
+        print(i, end=" ")
