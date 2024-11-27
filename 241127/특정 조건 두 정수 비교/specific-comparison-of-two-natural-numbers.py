3605cd51fc6147e466981bc082ed887b00ae408d
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 16:13:12 +0900

    Title: 특정 조건 두 정수 비교, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241127/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+
+a = arr[0]
+b = arr[1]
+
+#a<b
+if (a<b) :
+	print("1", end=" ")
+else :
+	print("0", end= " ")
+
+if (a==b) :
+	print("1", end=" ")
+else :
+	print("0")
