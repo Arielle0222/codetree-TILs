ade75a4ec64d4fbd589d324453a18d6679697483
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 16:20:46 +0900

    Title: 비교 연산, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241127/비교 연산/comparison-operator.py
@@ -0,0 +1,21 @@
+arr = list(map(int, input().split()))
+
+a = arr[0]
+b = arr[1]
+
+"""print(a>=b)
+print(a>b)
+print(a<=b)
+print(a<b)
+print(a==b)
+print(a!=b)
+-> 얘네들은 True/False 결과로 나옴
+"""
+
+
+print(int(a >= b))
+print(int(a > b))
+print(int(a <= b))
+print(int(a < b))
+print(int(a == b))
+print(int(a != b))
