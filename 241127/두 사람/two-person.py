b21095ab62df21edf22147192ea4094f86a61417
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-27 17:17:49 +0900

    Title: 두 사람, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241127/두 사람/two-person.py
+++ 241127/두 사람/two-person.py
@@ -5,9 +5,9 @@ arr_b = list(input().split())
 b_a = int(arr_b[0])
 b_g = arr_b[1]
 
-if ((a_a >=19 or b_a >=19) and (a_g == "M" or b_g == "M")):
-    print("1")
-else :
-    print("0")
+if (a_age >= 19 and a_gen == 'M') or (b_age >= 19 and b_gen == 'M'):
+	print("1")
+else:
+	print("0")
 
 
