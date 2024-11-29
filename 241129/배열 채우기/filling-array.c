05d51a8a0533b2964f3cdaceb485a4a6f10b8f23
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-11-29 20:48:10 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241129/배열 채우기/filling-array.c
+++ 241129/배열 채우기/filling-array.c
@@ -2,20 +2,22 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
+
     int arr[10];
+    int cnt=0;
 
     for (int i=0; i<10; i++){
-        scanf("%d", &arr[i]);
+        scanf("%d ", &arr[i]);
 
-        if (arr[i]==0) {
+        if (arr[i]==0){
             break;
         }
+        cnt++;
     }
-
-    for(int i=arr[i]+1; i>=0; i--){
-         printf("%d ", arr[i]);
+    //출력
+    for (int i=cnt-1; i>=0; i--){
+        printf("%d ", arr[i]); 
     }
-        
     
     return 0;
 }
