34e899793844ea6d4aa24a77cfd7e8c1229f4ea9
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-12-02 21:23:46 +0900

    Title: 1부터 특정 수까지의 합, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241202/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
+++ 241202/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
@@ -1,11 +1,12 @@
-N = int(input())
+
 
 def add(n) :
     sum_val = 0
     for i in range(1,n+1):
-        sum_val += i
+        sum_val+=i
     
     return sum_val // 10
 
+N = int(input())
 result = add(N)
-print(result) 
+print(result)
