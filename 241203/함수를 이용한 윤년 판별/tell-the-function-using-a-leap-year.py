9b0299ab26fe6c879a029e6b526c49e2b5fef442
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-12-03 12:42:43 +0900

    Title: 함수를 이용한 윤년 판별, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241203/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,32 @@
+def is_yun(n):
+    if (n%4==0) :
+        return True
+        if (n%100==0 and n%400!=0) :
+            return False
+        else : 
+            return True
+    else :
+        return False
+
+
+y = int(input())
+
+if is_yun(y):
+    print("true")
+else:
+    print("false")
+
+
+'''
+y = int (input())
+
+if (y%4==0) :
+	if (y%100==0 and y%400!=0) :
+		print("false") #윤년 아님
+	else :
+		print("true") #윤년
+else :
+	print("false") #윤년 아님
+
+
+'''
