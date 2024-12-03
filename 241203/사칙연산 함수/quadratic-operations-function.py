f7ae95f74b9f5f3655271b2fd164d25dbf7b2713
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-12-03 13:33:56 +0900

    Title: 사칙연산 함수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241203/사칙연산 함수/quadratic-operations-function.py
+++ 241203/사칙연산 함수/quadratic-operations-function.py
@@ -4,9 +4,7 @@
 '''
 
 def calculate(a,o,c):
-    if (o not in {'+', '-', '/', '*'}) :
-        print("False")
-    elif (o == '+') :
+    if (o == '+') :
         return a+c 
     elif (o == '-') :
         return a-c 
@@ -20,5 +18,8 @@ arr = list(input().split( ))
 a = int(arr[0])
 o = arr[1]
 c = int(arr[2])
+
+if (o not in {'+', '-', '/', '*'}) :
+        print("false")
 
 print(f"{a} {o} {c} =", calculate(a,o,c))
