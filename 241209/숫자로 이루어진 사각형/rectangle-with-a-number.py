a61c8ee2803fbf8eed01a0962a8c5887352e67ff
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2024-12-09 21:22:18 +0900

    Title: 숫자로 이루어진 사각형, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241209/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 241209/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -1,13 +1,14 @@
 n = int(input())
 
 def repeat():
-    cnt = 1  # Start counting from 1
-    for i in range(n):  # Repeat `n` times
-        print(cnt, end=" ")
-        cnt += 1
-        if cnt > 9:  # Cycle back to 1 after reaching 9
-            cnt = 1
-        if (i + 1) % 9 == 0:  # Print a new line after every 9 numbers
-            print()
+    for i in range(n):
+       cnt = 1
+        for _ in range(n):
+            for _ in range(n):
+                print(cnt, end=" ")
+                cnt += 1
+                if cnt == 10:
+                cnt = 1
+        print()
 
-repeat(
+repeat()
