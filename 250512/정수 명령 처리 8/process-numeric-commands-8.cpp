2d8b28bd84a1f5d9fe13884209c029a3cb17d450
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2025-05-12 00:27:36 +0900

    Title: 정수 명령 처리 8, Time: 8ms, Memory: 1MB, Status: Passed - Codetree

--- 250512/정수 명령 처리 8/process-numeric-commands-8.cpp
+++ 250512/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <list>
-
 using namespace std;
 
 int N;
@@ -9,7 +8,6 @@ string command[10000];
 int A[10000];
 
 int main() {
-    
     cin >> N;
 
     for (int i = 0; i < N; i++) {
@@ -19,44 +17,35 @@ int main() {
         }
     }
 
-    // Please write your code here.
-         
-    list<int> l; 
-    
-    for (int i=0; i<13; i++){
-        if (command[i] == "push_back"){
-            l.push_front[i];
-            cout << l.back() << endl;
-        }
-        else if(command[i] == "push_front"){
-            l.push_front[i];
-            cout << l.front() << endl;
-        }
-        else if (command[i] == "pop_front"){
-            l.pop_front[i];
-            cout << l.front() << endl;
+    list<int> l;
 
-        }
-        else if (command[i] == "pop_back"){
-            l.pop_back[i];
-            cout << l.back() << endl;
-        }
-        else if(command[i] == "size"){
-            cout << l.size() << endl;
-        }
-        else if(command[i] == "empty") {
-            if (l.empty()){
-                cout << 1;
-            }
+    for (int i = 0; i < N; i++) {
+        if (command[i] == "push_front") {
+            l.push_front(A[i]);
+        } else if (command[i] == "push_back") {
+            l.push_back(A[i]);
+        } else if (command[i] == "pop_front") {
+            if (l.empty()) cout << -1 << '\n';
             else {
-                cout << 0;
+                cout << l.front() << '\n';
+                l.pop_front();
             }
-        }
-        else if(command[i] == "front"){
-            cout << l.front() << endl;
-        }
-        else if(command[i] == "back"){
-            cout << l.back() << endl;
+        } else if (command[i] == "pop_back") {
+            if (l.empty()) cout << -1 << '\n';
+            else {
+                cout << l.back() << '\n';
+                l.pop_back();
+            }
+        } else if (command[i] == "size") {
+            cout << l.size() << '\n';
+        } else if (command[i] == "empty") {
+            cout << (l.empty() ? 1 : 0) << '\n';
+        } else if (command[i] == "front") {
+            if (l.empty()) cout << -1 << '\n';
+            else cout << l.front() << '\n';
+        } else if (command[i] == "back") {
+            if (l.empty()) cout << -1 << '\n';
+            else cout << l.back() << '\n';
         }
     }
 
