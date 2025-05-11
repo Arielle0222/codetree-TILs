2c6be36cfef326c39ab02f79734470b1e3cdb078
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2025-05-11 20:56:49 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250511/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    if(n < 0) {
+        cout << n << endl << "minus";
+    }
+    else {
+        cout << n;
+    }
+    return 0;
+}
