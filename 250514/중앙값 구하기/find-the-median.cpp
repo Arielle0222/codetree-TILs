1ba9b43f3ba5367be5d8d98840c1fc06e2a9f6d5
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2025-05-14 14:46:23 +0900

    Title: 중앙값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250514/중앙값 구하기/find-the-median.cpp
+++ 250514/중앙값 구하기/find-the-median.cpp
@@ -2,13 +2,38 @@
 using namespace std;
 
 int main() {
-    // Please write your code here.
+    // 변수 선언
+    int a, b, c;
 
-    int a,b,c;
+    // 입력
     cin >> a >> b >> c;
 
-    if (a>b && a<c) cout << a; //a is middle number 2 1 3 
-    else if (b>a && b<c) cout << b;              // 1 2 3
-    else if (c>a && c<b) cout << c;              // 1 3 2
+    // 출력
+    if(a > b) {
+        if(c > a) {
+            // a > b, c > a 일때 (c > a > b)
+            cout << a;
+        }
+        // a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+        else if(b > c) {
+            cout << b;
+        }
+        else {
+            cout << c;
+        }
+    }
+    else {
+        if(c > b) {
+            // b > a, c > b 일때 (c > b > a)
+            cout << b;
+        }
+        // b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+        else if(a > c) {
+            cout << a;
+        }
+        else {
+            cout << c;
+        }
+    }
     return 0;
 }
