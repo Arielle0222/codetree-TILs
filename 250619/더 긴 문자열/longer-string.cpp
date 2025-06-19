e5676ec4fcbe4accece6dbd456be17e499f60692
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2025-06-19 20:09:27 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250619/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string str_1;
+    string str_2;
+
+    cin >> str_1 >> str_2;
+
+    if (str_1.length() > str_2.length() ){
+        cout << str_1 << " " << str_1.length();
+    }
+    else{
+        cout << str_2 << " " << str_2.length();
+    }
+    return 0;
+}
