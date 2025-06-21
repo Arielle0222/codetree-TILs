0927701ef42a71bec25ef09cadab30c0507ded3e
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2025-06-21 22:20:11 +0900

    Title: 배수의 개수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250621/배수의 개수/number-of-multipliers.cpp
+++ 250621/배수의 개수/number-of-multipliers.cpp
@@ -4,22 +4,20 @@ using namespace std;
 int main() {
     // Please write your code here.
 
-    int total_3, total_5;
-    //int n;
+    int n;
+    int total_3 = 0, total_5 = 0;
 
-    for(int i=1; i<=10; i++){
-        int n;
+    for (int i=1; i<=10; i++){
         cin >> n;
 
         if (n%3 == 0) {
             total_3 ++;
         }
-        else if (n%5 == 0) {
+        if (n%5 == 0) {
             total_5 ++;
         }
     }
 
     cout << total_3 << " " << total_5;
-
     return 0;
 }
