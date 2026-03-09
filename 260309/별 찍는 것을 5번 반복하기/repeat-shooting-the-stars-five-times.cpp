0bf6ce6f84b2f9416d8e71ffb01b891d17a27cdc
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2026-03-09 16:25:54 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Print_starts10(){
+        for (int i=0; i<10; i++){
+            cout << "*";
+        }
+        cout << endl;
+        
+}
+
+int main() {
+    // Please write your code here.
+    for (int i=0; i<5; i++) {
+        Print_starts10();
+    }
+    return 0;
+}
