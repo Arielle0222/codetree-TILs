f758099a3428d4107ffb71aa67570eaf04211434
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2026-03-09 16:33:44 +0900

    Title: 반복 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260309/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+//int N;
+void Make_string(int N){
+    for (int i=0; i<N; i++){
+        cout << "12345^&*()_";
+    }
+    cout << endl;
+}
+
+int main() {
+    int M;
+    cin >> N;
+
+    // Please write your code here.
+
+    Make_string(N);
+
+    return 0;
+}
