683af7103afcb0cc619c6209b86f7b131428335e
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2026-03-09 16:51:08 +0900

    Title: 숫자로 이루어진 사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260309/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
+++ 260309/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -6,8 +6,9 @@ void Make_int_square(int n){
     int cnt=1;
 
     for (int i=0; i<n; i++){
-        for(int j=1; j<n; j++){
+        for(int j=10 j<n; j++){
             cout << cnt << " ";
+            cnt++;
             if(cnt==10){
                 cnt=1;
             }
