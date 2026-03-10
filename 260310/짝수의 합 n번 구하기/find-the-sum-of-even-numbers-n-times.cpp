47a795c6200003d596a1dec44e285a954b3cfbf3
SeoHee Kim / Arielle <114340019+Arielle0222@users.noreply.github.com>
2026-03-10 19:47:44 +0900

    Title: 짝수의 합 n번 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260310/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
+++ 260310/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -3,25 +3,21 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-
     int n;
     cin >> n;
-    
-    for (int i=0; i<n; i++){
-        int a,b;
+    for(int i=0; i<n; i++){
         int result = 0;
 
+        int a,b;
         cin >> a >> b;
 
-        for (int i=a; i<=b; i++){
+        for(int i=a; i<b+1; i++){
             if (i%2==0){
-                result+=i;
+                result +=i;
             }
+        //cout << result << endl;
         }
-        cout << result << endl; 
+        cout << result << enld;
     }
-    
     return 0;
 }
-
-
